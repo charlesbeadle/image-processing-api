2439263c089a1cf1adedc2bdab35a125dd2b3424
Charles Beadle <charles.beadle@protonmail.com>
2023-03-05 10:04:19 -0500

    refactor: Add port variable fallback

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.use('/api', apiRouter);
 
