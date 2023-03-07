1fdf182da0f1c40f0189213e664372935c08481d
Charles Beadle <charles.beadle@protonmail.com>
2023-03-07 11:05:09 -0500

    refactor: Revise the imageExists unit test

--- src/tests/middleware/image-processing/imageProcessingSpec.ts
+++ src/tests/middleware/image-processing/imageProcessingSpec.ts
@@ -1,23 +1,19 @@
-import { Request, Response, NextFunction } from 'express';
+import type { Request, Response, NextFunction } from 'express';
 import { imageExists } from '../../../middleware/image-processing/imageExists';
 
-describe('Image Processing Middleware', () => {
-	let mockReq: Partial<Request>;
-	let mockRes: Partial<Response>;
-	let mockNextFunction: NextFunction;
-	beforeEach(() => {
-		mockReq = {
-			query: {
-				name: 'santamonica',
-			},
-		};
-		mockRes = {
-			status: jasmine.createSpy('status'),
-			send: jasmine.createSpy('send'),
-		};
-		mockNextFunction = jasmine.createSpy('next');
-	});
-	it('Calls the next middleware if the requested file name exists', async () => {
+describe('Image processing middleware', () => {
+	const mockReq: Partial<Request> = {
+		query: {
+			name: 'santamonica',
+		},
+	};
+	const mockRes: Partial<Response> = {
+		status: jasmine.createSpy('status'),
+		send: jasmine.createSpy('send'),
+	};
+	const mockNextFunction: Partial<NextFunction> = jasmine.createSpy('next');
+
+	it('calls the next middleware if the requested file name exists', async () => {
 		await imageExists(
 			mockReq as Request,
 			mockRes as Response,
