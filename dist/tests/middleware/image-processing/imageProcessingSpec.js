be161f7606dc8db0395f7d363e12ae6f91445ae1
Charles Beadle <charles.beadle@protonmail.com>
2023-03-08 10:02:17 -0500

    test: Add a test for the processImage middleware

--- dist/tests/middleware/image-processing/imageProcessingSpec.js
+++ dist/tests/middleware/image-processing/imageProcessingSpec.js
@@ -8,12 +8,29 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const imageExists_1 = require("../../../middleware/image-processing/imageExists");
+const processImage_1 = require("../../../middleware/image-processing/processImage");
+const promises_1 = require("node:fs/promises");
+const path_1 = __importDefault(require("path"));
+const imagesPath = path_1.default.resolve(__dirname, '../../../images');
 describe('Image processing middleware', () => {
+    beforeAll(() => __awaiter(void 0, void 0, void 0, function* () {
+        try {
+            yield (0, promises_1.access)(`${imagesPath}/santamonica-300.jpg`, promises_1.constants.F_OK);
+            yield (0, promises_1.unlink)(`${imagesPath}/santamonica-300.jpg`);
+        }
+        catch (_a) {
+            return;
+        }
+    }));
     const mockReq = {
         query: {
             name: 'santamonica',
+            size: '300',
         },
     };
     const mockRes = {
@@ -21,8 +38,21 @@ describe('Image processing middleware', () => {
         send: jasmine.createSpy('send'),
     };
     const mockNextFunction = jasmine.createSpy('next');
-    it('calls the next middleware if the requested file name exists', () => __awaiter(void 0, void 0, void 0, function* () {
+    it('calls the next function if the requested file name exists', () => __awaiter(void 0, void 0, void 0, function* () {
         yield (0, imageExists_1.imageExists)(mockReq, mockRes, mockNextFunction);
         expect(mockNextFunction).toHaveBeenCalled();
+    }));
+    it('resizes an image if one is not cached', () => __awaiter(void 0, void 0, void 0, function* () {
+        yield (0, processImage_1.processImage)(mockReq, mockRes, mockNextFunction);
+        const resizedExists = () => __awaiter(void 0, void 0, void 0, function* () {
+            try {
+                yield (0, promises_1.access)(`${imagesPath}/santamonica-300.jpg`, promises_1.constants.F_OK);
+                return true;
+            }
+            catch (_b) {
+                return false;
+            }
+        });
+        expect(yield resizedExists()).toBe(true);
     }));
 });
--- src/tests/middleware/image-processing/imageProcessingSpec.ts
+++ src/tests/middleware/image-processing/imageProcessingSpec.ts
@@ -1,10 +1,23 @@
 import type { Request, Response, NextFunction } from 'express';
 import { imageExists } from '../../../middleware/image-processing/imageExists';
+import { processImage } from '../../../middleware/image-processing/processImage';
+import { access, constants, unlink } from 'node:fs/promises';
+import path from 'path';
+const imagesPath = path.resolve(__dirname, '../../../images');
 
 describe('Image processing middleware', () => {
+	beforeAll(async () => {
+		try {
+			await access(`${imagesPath}/santamonica-300.jpg`, constants.F_OK);
+			await unlink(`${imagesPath}/santamonica-300.jpg`);
+		} catch {
+			return;
+		}
+	});
 	const mockReq: Partial<Request> = {
 		query: {
 			name: 'santamonica',
+			size: '300',
 		},
 	};
 	const mockRes: Partial<Response> = {
@@ -13,12 +26,29 @@ describe('Image processing middleware', () => {
 	};
 	const mockNextFunction: Partial<NextFunction> = jasmine.createSpy('next');
 
-	it('calls the next middleware if the requested file name exists', async () => {
+	it('calls the next function if the requested file name exists', async () => {
 		await imageExists(
 			mockReq as Request,
 			mockRes as Response,
 			mockNextFunction as NextFunction
 		);
 		expect(mockNextFunction).toHaveBeenCalled();
+	});
+
+	it('resizes an image if one is not cached', async () => {
+		await processImage(
+			mockReq as Request,
+			mockRes as Response,
+			mockNextFunction as NextFunction
+		);
+		const resizedExists = async () => {
+			try {
+				await access(`${imagesPath}/santamonica-300.jpg`, constants.F_OK);
+				return true;
+			} catch {
+				return false;
+			}
+		};
+		expect(await resizedExists()).toBe(true);
 	});
 });
